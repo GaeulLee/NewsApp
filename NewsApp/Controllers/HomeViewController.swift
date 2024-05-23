6d009d749e4876cbea6fbe6f5fb5911583a6e5ea
gaeulLee <rhakq23@gmail.com>
2024-05-23 22:42:28 +0900

    testing UIElements

---
+++ NewsApp/Controllers/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  NewsApp
+//
+//  Created by 이가을 on 5/23/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Home"
+
+        
+    }
+    
+
+    @IBAction func searchButtonTapped(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "HomeToSearch", sender: self)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
